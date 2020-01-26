73bf662fa04fa8e30e143b48b4409373c0487d96
ngel rojas p <aomarrojasp@gmail.com>
2020-01-26 17:28:29 -0300

    add models and controllers

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,12 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use App\User;
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -23,5 +28,39 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PROUDCT]),
+        'image' => $faker->randomElement(['1.jpeg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->radom(); 
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer-id,
+        'product_id' => $seller->products->random()->id,
+
     ];
 });
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,22 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/* buyers*/
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/*categories*/
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/*products*/
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+/*sellers*/
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+/*transactions*/
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+/*users*/
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
